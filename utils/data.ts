972afb5e70d5e70559e874bad1e03c22247aa625
Belle Aerni <belleaerni@gmail.com>
2022-12-16 10:14:33 -0800

    & update the URL

--- utils/data.ts
+++ utils/data.ts
@@ -11,7 +11,7 @@ export const extensionData: Extension[] = [
       name: 'Scith & the FOSSBilling Team',
       URL: 'https://fossbilling.org'
     },
-    download_url: 'https://github.com/FOSSBilling/Proxmox/releases/download/0.0.2/Serviceproxmox.zip',
+    download_url: 'https://github.com/FOSSBilling/Proxmox/releases/download/0.0.3/Serviceproxmox.zip',
     version: '0.0.3',
     license: 'GPLv3',
     icon_url: 'https://raw.githubusercontent.com/FOSSBilling/Proxmox/main/icon.png'
