c7a20ebc62c1de22d04774e8459b6a15cd39f325
Yağızhan <hi@yagiz.dev>
2023-03-31 11:22:09 +0300

    Update data.ts

--- data/data.ts
+++ data/data.ts
@@ -3,20 +3,6 @@ import { Extension } from '../interfaces'
 /** Dummy extension data. We probably should migrate to a database as these get bigger. */
 export const extensionData: Extension[] = [
   {
-    id: 'Serviceproxmox',
-    type: 'mod',
-    name: 'Proxmox',
-    description: 'Provision Proxmox VMs using the Proxmox API',
-    author: {
-      name: 'Scith & the FOSSBilling Team',
-      URL: 'https://fossbilling.org'
-    },
-    download_url: 'https://github.com/FOSSBilling/Proxmox/releases/download/0.0.4/Serviceproxmox.zip',
-    version: '0.0.4',
-    license: 'GPLv3',
-    icon_url: 'https://raw.githubusercontent.com/FOSSBilling/Proxmox/main/icon.png'
-  },
-  {
     id: 'Example',
     type: 'mod',
     name: 'Example',
@@ -29,5 +15,19 @@ export const extensionData: Extension[] = [
     version: '0.0.1',
     license: 'Apache-2.0',
     icon_url: 'https://raw.githubusercontent.com/FOSSBilling/example-module/main/icon.svg'
+  },
+  {
+    id: 'Serviceproxmox',
+    type: 'mod',
+    name: 'Proxmox',
+    description: 'Provision Proxmox VMs using the Proxmox API',
+    author: {
+      name: 'Scith & the FOSSBilling Team',
+      URL: 'https://fossbilling.org'
+    },
+    download_url: 'https://github.com/FOSSBilling/Proxmox/releases/download/0.0.4/Serviceproxmox.zip',
+    version: '0.0.4',
+    license: 'GPLv3',
+    icon_url: 'https://raw.githubusercontent.com/FOSSBilling/Proxmox/main/icon.png'
   },
 ]
