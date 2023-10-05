f30d95c23eb79ef5ef808a6316102d66954c2716
Belle Aerni <belleaerni@gmail.com>
2023-10-05 13:38:15 -0700

    Fixed the readme for the example module

--- data/extensions.ts
+++ data/extensions.ts
@@ -60,61 +60,64 @@ export const extensionData: Extension[] = [
     },
     icon_url: "https://raw.githubusercontent.com/FOSSBilling/example-module/main/src/icon.svg",
     website: "https://fossbilling.org",
-    readme: `# Example module README file
+    readme: 
+    `# Example module README file
 
-    This module provides a starting point for the developers on creating their FOSSBilling module.
+This module provides a starting point for the developers on creating their FOSSBilling module.
     
-    Explore the files and comments in the code to understand the structure of the module better. See the social links on [our website](https://fossbilling.org) if you need further information. This module has its own [GitHub repository](https://github.com/FOSSBilling/example-module) where you can submit issues and pull requests.
+Explore the files and comments in the code to understand the structure of the module better. See the social links on [our website](https://fossbilling.org) if you need further information. This module has its own [GitHub repository](https://github.com/FOSSBilling/example-module) where you can submit issues and pull requests.
     
-    In general, we use modules to extend the functionality of FOSSBilling.
+In general, we use modules to extend the functionality of FOSSBilling.
     
-    All modules can communicate with the other modules using their API endpoints.
+All modules can communicate with the other modules using their API endpoints.
     
-    ## Technical requirements about modules
+## Technical requirements about modules
     
-    ## Required
+## Required
     
-    * Module folder has to contain a **manifest.json** file to describe itself. The module engine will look for this file to find information about your extension.
+ - Module folder has to contain a **manifest.json** file to describe itself. The module engine will look for this file to find information about your extension.
     
-    ## Optional
+## Optional
     
-    * **README.md** - A file which generally is used to hold a getting started guide or installation instructions for your module.
-    * **html_admin** - A folder holding front-end templates (\`*.html.twig files\`) for the administrator panel.
-    * **html_client** - A folder holding front-end templates (\`*.html.twig files\`) for the client / guest area.
+ - **README.md** - A file which generally is used to hold a getting started guide or installation instructions for your module.
+ - **html_admin** - A folder holding front-end templates (\`*.html.twig files\`) for the administrator panel.
+ - **html_client** - A folder holding front-end templates (\`*.html.twig files\`) for the client / guest area.
     
-    ### Controller folder
+### Controller folder
     
-    * **Admin.php** - Defines the module's routes and navigation items for the administrator panel.
-    * **Client.php** - Used to define the module's routes for the client / guest area.
+ - **Admin.php** - Defines the module's routes and navigation items for the administrator panel.
+ - **Client.php** - Used to define the module's routes for the client / guest area.
+
+### Api folder
     
-    ### Api folder
+ - **Admin.php** - Administrator API, only authorized administrators will be able to call these endpoints.
+ - **Client.php** - Client API, only logged in clients will be able to call these endpoints.
+ - **Guest.php** - Guest API, no authorization is needed for these endpoints. Don't provide confidential data over these endpoints. Anybody over the internet will be able to access these information, including bots.
     
-    * **Admin.php** - Administrator API, only authorized administrators will be able to call these endpoints.
-    * **Client.php** - Client API, only logged in clients will be able to call these endpoints.
-    * **Guest.php** - Guest API, no authorization is needed for these endpoints. Don't provide confidential data over these endpoints. Anybody over the internet will be able to access these information, including bots.
+## Tips
     
-    ## Tips
+We recommend hosting your extensions on a public [GitHub](https://github.com) repository.
     
-    We recommend hosting your extensions on a public [GitHub](https://github.com) repository.
+### Automated compatibility checking
     
-    ### Automated compatibility checking
+As FOSSBilling evolves and matures, its internal functionality changes, which can create compatibility issues between your module and FOSSBilling.
+To help developers catch these issues early on, we've designed a workflow that enables you to perform a PHPStan analysis of your module with both the latest FOSSBilling release and its preview builds.
+While PHPStan cannot perform live tests, it's a useful tool to verify that your module doesn't reference missing functions, use incorrect types, or have other common low-level issues.
     
-    As FOSSBilling evolves and matures, its internal functionality changes, which can create compatibility issues between your module and FOSSBilling.
-    To help developers catch these issues early on, we've designed a workflow that enables you to perform a PHPStan analysis of your module with both the latest FOSSBilling release and its preview builds.
-    While PHPStan cannot perform live tests, it's a useful tool to verify that your module doesn't reference missing functions, use incorrect types, or have other common low-level issues.
+#### Setup
     
-    #### Setup
+More in-depth instructions are planned. For now, check out the required files:
     
-    More in-depth instructions are planned. For now, check out the required files:
+ - [php-ci.yml](https://github.com/FOSSBilling/example-module/blob/main/.github/workflows/php-ci.yml)
+ - [phpstan.neon](https://github.com/FOSSBilling/example-module/blob/main/phpstan.neon)
     
-    * [php-ci.yml](https://github.com/FOSSBilling/example-module/blob/main/.github/workflows/php-ci.yml)
-    * [phpstan.neon](https://github.com/FOSSBilling/example-module/blob/main/phpstan.neon)
+## Licensing
     
-    ## Licensing
-    This extension is open source software and is released under the Apache v2.0 license. See [LICENSE](LICENSE) for the full license terms.
+This extension is open source software and is released under the Apache v2.0 license. See [LICENSE](LICENSE) for the full license terms.
     
-    This product includes the following third party work:
-    * Open Source Iconography by [Pictogrammers](https://pictogrammers.com/) licensed under the [Pictogrammers Free License](https://pictogrammers.com/docs/general/license/).`,
+This product includes the following third party work:
+    
+ - Open Source Iconography by [Pictogrammers](https://pictogrammers.com/) licensed under the [Pictogrammers Free License](https://pictogrammers.com/docs/general/license/).`,
   },
   /*{
     id: "Serviceproxmox",
