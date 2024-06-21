763db6200c96f54ea25b0ee267f64ecf7ed978ee
Neto <tynhomello@gmail.com>
2024-06-21 20:02:07 -0300

    Added PAYEER payment gateway extension

--- data/extensions.ts
+++ data/extensions.ts
@@ -584,4 +584,54 @@ For support or questions, feel free to contact me at albinvar@pm.me
   This FOSSBilling BTCPay Payment Gateway Integration module is open-source software licensed under the [Apache License 2.0](LICENSE).
   `,
   },
+  {
+    id: "PAYEER",
+    type: "payment-gateway",
+    name: "PAYEER",
+    description: "PAYEER extension for FOSSBilling",
+    author: findAuthorByID("neto737"),
+    license: {
+      name: "MIT",
+      URL: "https://github.com/neto737/PAYEER-FOSSBilling/blob/main/LICENSE",
+    },
+    source: {
+      type: "github",
+      repo: "neto737/PAYEER-FOSSBilling",
+    },
+    version: "0.1.0",
+    download_url:
+        "https://github.com/neto737/PAYEER-FOSSBilling/releases/download/0.1.0/PAYEER.zip",
+    releases: [
+      {
+        tag: "0.1.0",
+        date: "2024-06-21T22:41:54Z",
+        download_url:
+            "https://github.com/neto737/PAYEER-FOSSBilling/releases/download/0.1.0/PAYEER.zip",
+        changelog_url:
+            "https://github.com/neto737/PAYEER-FOSSBilling/commits/0.1.0",
+        min_fossbilling_version: "0.6",
+      },
+    ],
+    icon_url:
+        "https://raw.githubusercontent.com/neto737/PAYEER-FOSSBilling/main/payeer-logo.png",
+    website: "https://payeer.com/",
+    readme: ` # PAYEER for FOSSBilling
+  Quickly and easily integrate [PAYEER](https://payeer.com) into your [FOSSBilling](https://fossbilling.org) instance using this extension.
+
+  ## Installation
+
+  ### Manual installation
+  1. Download the latest release from the [releases](https://github.com/neto737/PAYEER-FOSSBilling/releases/latest)
+  2. Create a new folder named `PAYEER` in the `/library/Payment/Adapter` directory of your FOSSBilling installation
+  3. Extract the archive you've downloaded in the first step into the new directory
+  4. Go to the "Payment gateways" page in your admin panel (under the "System" menu in the navigation bar) and find PAYEER in the "New payment gateway" tab
+  5. Click the cog icon next to PAYEER to install and configure PAYEER
+
+  ## Licensing
+  This extension is licensed under the MIT license. See the [LICENSE](LICENSE) file for more information.
+
+  ## Disclaimer
+  This extension is not affiliated with PAYEER Limited. in any way. PAYEER is a registered trademark of PAYEER Limited.
+  `,
+  },
 ];
