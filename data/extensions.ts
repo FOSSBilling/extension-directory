eee5b2864d6eae75e9bd6bb3e31b0c6e38372f3c
Neto <tynhomello@gmail.com>
2025-05-30 20:32:37 +0930

    Add FaucetPay payment gateway (#171)
    * Add FaucetPay payment gateway
    
    * Fix extensions.ts
    
    * Fix extensions.ts
    
    ---------
    
    Co-authored-by: Adam Daley <admdly@users.noreply.github.com>

--- data/extensions.ts
+++ data/extensions.ts
@@ -879,6 +879,60 @@ For support or questions, feel free to contact me at albinvar@pm.me
   Sales Support:sales@netim.com`,
 },
 {
+    id: "FaucetPay",
+    type: "payment-gateway",
+    name: "FaucetPay",
+    description: "FaucetPay extension for FOSSBilling",
+    author: findAuthorByID("neto737"),
+    license: {
+      name: "MIT",
+      URL: "https://github.com/neto737/FaucetPay-FOSSBilling/blob/main/LICENSE",
+    },
+    source: {
+      type: "github",
+      repo: "neto737/FaucetPay-FOSSBilling",
+    },
+    version: "0.1.0",
+    download_url:
+        "https://github.com/neto737/FaucetPay-FOSSBilling/releases/download/0.1.0/FaucetPay-v0.1.0.zip",
+    releases: [
+      {
+        tag: "0.1.0",
+        date: "2024-11-23T01:37:45Z",
+        download_url:
+            "https://github.com/neto737/FaucetPay-FOSSBilling/releases/download/0.1.0/FaucetPay-v0.1.0.zip",
+        changelog_url:
+            "https://github.com/neto737/FaucetPay-FOSSBilling/releases/tag/0.1.0",
+        min_fossbilling_version: "0.6",
+      },
+    ],
+    icon_url:
+        "https://raw.githubusercontent.com/neto737/FaucetPay-FOSSBilling/main/faucetpay-logo.png",
+    website: "https://faucetpay.io/",
+    readme: ` # FaucetPay for FOSSBilling
+  Quickly and easily integrate [FaucetPay](https://faucetpay.io) into your [FOSSBilling](https://fossbilling.org) instance using this extension.
+
+  ## Installing the Plugin
+  ### 1). Extension directory
+
+  The easiest way to install this extension is by using the [FOSSBilling extension directory](https://extensions.fossbilling.org/extension/FaucetPay).
+
+  ### 2). Manual installation
+  
+  1. Download the latest release from the [releases](https://github.com/neto737/FaucetPay-FOSSBilling/releases/latest)
+  2. Create a new folder named \`FaucetPay\` in the \`/library/Payment/Adapter\` directory of your FOSSBilling installation
+  3. Extract the archive you've downloaded in the first step into the new directory
+  4. Go to the "Payment gateways" page in your admin panel (under the "System" menu in the navigation bar) and find FaucetPay in the "New payment gateway" tab
+  5. Click the cog icon next to FaucetPay to install and configure FaucetPay
+
+  ## Licensing
+  This extension is licensed under the MIT license. See the [LICENSE](LICENSE) file for more information.
+
+  ## Disclaimer
+  This extension is not affiliated with Basilisk Entertainment S.R.L in any way. FaucetPay is a registered trademark of Basilisk Entertainment S.R.L.
+  `,
+  },
+  {
   id: "OpenProvider",
   type: "domain-registrar",
   name: "OpenProvider",
